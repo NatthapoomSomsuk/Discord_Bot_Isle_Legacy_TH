8b9759758eef245df4c614a26b71c1ae2b99c0d9
Dragon Style Studio <natthapoom.somsuk@gmail.com>
2024-01-30 21:49:31 +0700

    The Isle TH Bot

--- config/config.js
+++ config/config.js
@@ -6,7 +6,6 @@ module.exports = {
     colorsuccess: "#77b255",
     colorfail: "#dd2e44",
     colorwarning: "#f6b26b",
-
-    userdatabasepath: `../TheIsle/Saved/Databases/Survival/Players`,
-    token: "NzEyNTc5ODMzNzc5MDYwNzQ2.Gl_hUP.uNeADH18YgcLmK4ZwFnC_oatf3_iOBgm5xHHiA", //โทเคนบอท
+    userdatabasepath: `../TheIsle/Saved/Databases/Survival/Players`,//ตำแหน่งไฟล์ข้อมูล ผู้เล่น
+    token: "<TOKEN BOT>", //โทเคนบอท Discord
 };
